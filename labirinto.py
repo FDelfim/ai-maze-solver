f2d9809d9d1da1d5e8abf59b23c0b2307998bbc7
henriquemalvar <henriquemalvar@gmail.com>
2022-09-12 20:29:43 -0300

    A Star Solution

--- labirinto.py
+++ labirinto.py
@@ -4,25 +4,31 @@
 
 import sys
 import time
+from scipy.spatial.distance import cityblock
 
 # Classe No com 3 atributos: estado, pai e ação
+
+
 class No():
-    def __init__(self, estado, pai, acao):
+    def __init__(self, estado, pai, acao, custo):
         self.estado = estado
         self.pai = pai
         self.acao = acao
+        self.custo = custo
 
 # Classe para tratar Nós Fronteira
 # Deep First Search (DFS)
+
+
 class PilhaFronteira():
     # Inicializa Fronteira vazia
     def __init__(self):
         self.fronteira = []
-    
-    # Insere na pilha	
+
+    # Insere na pilha
     def add(self, no):
         self.fronteira.append(no)
-    
+
     # Procura no pilha por um estado
     def contem_estado(self, estado):
         return any(no.estado == estado for no in self.fronteira)
@@ -42,6 +48,8 @@ class PilhaFronteira():
 
 # Breadth First Search (BFS) herdando métodos da DFS
 # Só muda a remoção do nó da fronteira
+
+
 class FilaFronteira(PilhaFronteira):
 
     # Remove estado da Fronteira do tipo Fila
@@ -53,7 +61,32 @@ class FilaFronteira(PilhaFronteira):
             self.fronteira = self.fronteira[1:]
             return no
 
+
+class AStarFronteira(PilhaFronteira):
+    def __init__(self, inicio, objetivo):
+        super().__init__()
+        self.inicio = inicio
+        self.objetivo = objetivo
+
+    def heuristic(self, estado):
+        return cityblock(estado, self.objetivo)
+
+    def function_avaliation(self, no):
+        print(f'g(n)= {no.custo} + h(n)= {self.heuristic(no.estado)} = f(n)= {self.heuristic(no.estado) + no.custo}')
+        return no.custo + self.heuristic(no.estado)
+
+    def remove(self):
+        if self.empty():
+            raise Exception("fronteira vazia")
+        else:
+            no = min(self.fronteira, key=self.function_avaliation)
+            self.fronteira.remove(no)
+            return no
+
+
 # Classe do Problema de Busca
+
+
 class Labirinto():
 
     # Inicializa instância do problema com o arquivo TXT filename
@@ -65,7 +98,8 @@ class Labirinto():
 
         # Valida Largada e Chegada
         if contents.count("A") != 1:
-            raise Exception("labirinto deve ter exatamente um ponto de partida")
+            raise Exception(
+                "labirinto deve ter exatamente um ponto de partida")
         if contents.count("B") != 1:
             raise Exception("labirinto deve ter exatamente uma chegada")
 
@@ -115,8 +149,8 @@ class Labirinto():
             print()
         print()
 
+    # Identifica os vizinhos do estado
 
-    # Identifica os vizinhos do estado 
     def vizinhos(self, estado):
         linha, coluna = estado
         candidatos = [
@@ -132,8 +166,8 @@ class Labirinto():
                 resultado.append((acao, (l, c)))
         return resultado
 
+    # Invoca o método solve() para encontrar a solução
 
-    # Invoca o método solve() para encontrar a solução 
     def solve(self):
         """Encontrar uma solução para labirinto, se existe."""
 
@@ -141,8 +175,11 @@ class Labirinto():
         self.num_explored = 0
 
         # Inicializa a fronteira apenas para o posição inicial
-        inicio = No(estado=self.inicio, pai=None, acao=None)
-        fronteira = PilhaFronteira() #Pilha -> Profundidade
+        inicio = No(estado=self.inicio, pai=None, acao=None, custo=0)
+        # No(estado=self.objetivo, pai=None, acao=None)
+        objetivo = self.objetivo
+        fronteira = AStarFronteira(
+            inicio, objetivo)  # Pilha -> Profundidade
         fronteira.add(inicio)
 
         # Inicializa um conjunto vazio de estados não explorados
@@ -178,7 +215,7 @@ class Labirinto():
             # Adiciona vizinhos a fronteira
             for acao, estado in self.vizinhos(no.estado):
                 if not fronteira.contem_estado(estado) and estado not in self.explored:
-                    filho = No(estado=estado, pai=no, acao=acao)
+                    filho = No(estado=estado, pai=no, acao=acao, custo=no.custo+1)
                     fronteira.add(filho)
 
     # Imprime o labirinto com os estados explorados
@@ -234,7 +271,7 @@ class Labirinto():
 
 
 # ----------------------
-# Programa Principal 
+# Programa Principal
 # ----------------------
 
 if len(sys.argv) != 2:
@@ -248,12 +285,11 @@ print("Solucionando...")
 t1 = time.time()
 m.solve()
 t2 = time.time()
-tempo_execucao = t2 - t1 
+tempo_execucao = t2 - t1
 print("Tempo de Execução: ", tempo_execucao)
 
 print("Estados Explorados:", m.num_explored)
 print("Solução: ")
 
-
-     m.print()
+m.print()
 m.output_image("labirinto.png", show_explored=True)
